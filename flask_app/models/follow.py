97084e7c9bff8e26ee4f7fd65fde9873340e8f55
Wiji <peawseaw@gmail.com>
2022-03-25 08:45:31 -0700

    update more features

--- flask_app/models/follow.py
+++ flask_app/models/follow.py
@@ -17,3 +17,29 @@ class Follow:
         query = "INSERT INTO follows (following_id, follower_id, updated_at, created_at) VALUES (%(following_id)s, %(follower_id)s, NOW(), NOW());"
         results = connectToMySQL(DB).query_db(query, data)
         return results
+
+    @classmethod
+    def get_follower(cls, data):
+        query = "SELECT * FROM follows WHERE following_id = %(following_id)s and follower_id = %(follower_id)s;"
+        results = connectToMySQL(DB).query_db(query,data)
+        if results:
+            one_user = cls(results[0])
+            print('this is the dictionary', results[0])
+            print('this is the object', one_user)
+            return one_user
+
+    @classmethod
+    def get_following(cls, data):
+        query = "SELECT * FROM follows WHERE following_id = %(following_id)s and follower_id = %(follower_id)s;"
+        results = connectToMySQL(DB).query_db(query,data)
+        if results:
+            one_user = cls(results[0])
+            print('this is the dictionary', results[0])
+            print('this is the object', one_user)
+            return one_user
+    
+    @classmethod
+    def delete_following(cls, data):
+        query = "DELETE FROM follows WHERE following_id = %(following_id)s and follower_id = %(follower_id)s;"
+        results = connectToMySQL(DB).query_db(query, data)
+        return results
