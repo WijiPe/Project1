eb8225ee7efee4fd906a2194f6f96f8177035991
Wiji <peawseaw@gmail.com>
2022-08-13 09:16:00 -0700

    fix css route

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask, session
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
+app.static_url_path = '/mywatchlist/static'
+url_map = app.url_map
+for rule in url_map.iter_rules('static'):
+    url_map._rules.remove(rule)
+app.add_url_rule(app.static_url_path + '/<path:filename>',endpoint='static', view_func=app.send_static_file)
+
