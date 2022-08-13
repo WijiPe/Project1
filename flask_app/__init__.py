e74832c34eddd7039a27904c1d3b5df8a61a2c0d
Wiji <peawseaw@gmail.com>
2022-08-13 09:24:06 -0700

    more code

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -6,5 +6,6 @@ url_map = app.url_map
 for rule in url_map.iter_rules('static'):
     url_map._rules.remove(rule)
 app.url_map._rules_by_endpoint['static'] = []
+app.view_functions["static"] = None
 app.add_url_rule(app.static_url_path + '/<path:filename>',endpoint='static', view_func=app.send_static_file)
 
