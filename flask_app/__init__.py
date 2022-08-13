1150eeeef5e353dd0cee41cee3abce3a26709ae0
Wiji <peawseaw@gmail.com>
2022-08-13 09:21:32 -0700

    mode code

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,5 +5,6 @@ app.static_url_path = '/mywatchlist/static'
 url_map = app.url_map
 for rule in url_map.iter_rules('static'):
     url_map._rules.remove(rule)
+app.url_map._rules_by_endpoint['static'] = []
 app.add_url_rule(app.static_url_path + '/<path:filename>',endpoint='static', view_func=app.send_static_file)
 
